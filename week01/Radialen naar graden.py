f7e9f76faa2ee0924d7b6dc7264e7ea3eecf5b5c
Luquatic <jesseyfransen@live.nl>
2016-11-26 19:21:10 +0100

    changed radialen naar graden

--- week01/Radialen naar graden.py
+++ week01/Radialen naar graden.py
@@ -5,18 +5,27 @@ https://dodona.ugent.be/nl/exercises/975017137/
 
 import math
 
-aantalRadiaal = input("Aantal radialen:\n")
+def main():
+    # User input
+    aantalRadiaal = input("Aantal radialen:\n")
 
-graden = math.degrees(aantalRadiaal)
+    # Graden berekenen
+    graden = math.degrees(aantalRadiaal)
+
+    # Graden naar GMS omzetten
+    gradenToGMS(graden)
 
 def gradenToGMS(graden):
     isPositive = graden >= 0
     minutes,seconds = divmod(graden*3600,60)
     degrees, minutes = divmod(minutes,60)
     degrees = degrees if isPositive else -degrees
+
+    # Uitkomst printen
     print (int(degrees))
     print (int(minutes))
     print (int(seconds))
 
-gradenToGMS(graden)
+if __name__ == "__main__":
+    main()
 
