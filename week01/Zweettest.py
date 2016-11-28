aae70d3face4317b57bd71892de666b52b7ddf96
Luquatic <jesseyfransen@live.nl>
2016-11-28 13:14:57 +0100

    added opdracht 7

--- week01/Zweettest.py
+++ week01/Zweettest.py
@@ -2,6 +2,7 @@
 Opdracht 6 - Zweettest
 https://dodona.ugent.be/nl/exercises/1173716008/
 """
+
 def main():
     # User input
     leeftijdInMaanden = float(input("Leeftijd in maanden: "))
---
+++ week02/Interessante getallen.py
@@ -0,0 +1,8 @@
+"""
+Opdracht 7 - Interessante getallen
+https://dodona.ugent.be/nl/exercises/211647828/
+"""
+
+def main():
+    # User input
+    t = float(input("Invoer t: "))
