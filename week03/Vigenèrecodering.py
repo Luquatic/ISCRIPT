4d04e79d6f136fa72fb918cf190e1f751eb67f32
Luquatic <jesseyfransen@live.nl>
2016-12-07 22:30:52 +0100

    added Vigenèrecoding

--- week03/Vigenèrecodering.py
+++ week03/Vigenèrecodering.py
@@ -4,61 +4,83 @@ https://dodona.ugent.be/nl/exercises/862295437/
 """
 
 import string
-tekstVersleuteld = []
-
-def main():
-    # Codeer 1
-    codeer('NOBODY EXPECTS THE SPANISH INQUISITION!', 'CIRCUS')
-
-    # # Decodeer 1
-    # decodeer('PWSQXQ MORYUVA VBW AGCHAUP KHIWQJKNAQV!', 'CIRCUS')
-    #
-    # # Codeer 2
-    # codeer('OH SHUT UP! AND GO AND CHANGE YOUR ARMOUR!', 'ARTHUR')
-    #
-    # # Decodeer 2
-    # decodeer('OY ZBLT NW! AEW AF RGK THRGNY YFNY RRDHBL!', 'ARTHUR')
-
 
 def codeer(tekst: str, sleutelwoord: str) -> None:
     # Verkrijg lengte van tekst
-    tekstLengte, newTekst = getLengteTekst(tekst)
+    tekstLengte = getLengteTekst(tekst)
     # Verkrijg lengte van sleutelwoord
     sleutelwoordLengte = getLengteSleutelwoord(sleutelwoord)
     # Kijkt hoevaak het sleutelwoord in de tekst kan en slaat de overige letters op
     aantalkeer_passend, overige_letters = divmod(tekstLengte, sleutelwoordLengte)
     # Verkrijg sleutelzin met de lengte van tekst
     sleutelZin = getSleutelzin(sleutelwoord, aantalkeer_passend, overige_letters)
+    # Verander tekst naar versleutelde tekst
+    tekstVersleuteld = getTekstVersleuteld(tekst, sleutelZin)
+    print(''.join(tekstVersleuteld))
 
+def decodeer(tekst: str, sleutelwoord: str) -> None:
+    # Verkrijg lengte van tekst
+    tekstLengte = getLengteTekst(tekst)
+    # Verkrijg lengte van sleutelwoord
+    sleutelwoordLengte = getLengteSleutelwoord(sleutelwoord)
+    # Kijkt hoevaak het sleutelwoord in de tekst kan en slaat de overige letters op
+    aantalkeer_passend, overige_letters = divmod(tekstLengte, sleutelwoordLengte)
+    # Verkrijg sleutelzin met de lengte van tekst
+    sleutelZin = getSleutelzin(sleutelwoord, aantalkeer_passend, overige_letters)
+    # Verander tekst naar versleutelde tekst
+    tekstUnlocked = getTekstUnlocked(tekst, sleutelZin)
+    print(''.join(tekstUnlocked))
+
+def getTekstVersleuteld(tekst, sleutelZin):
+    tekstVersleuteld = []
     i = 0
-    while i < tekstLengte:
+    while i < len(tekst):
         # Verkrijg letter van string
-        letterTekst = newTekst[i]
-        # Transform eerste letter naar cijfer
-        Oi = string.ascii_uppercase.index(letterTekst)
+        letterTekst = tekst[i]
 
-        # Verkrijg eerste letter van string
-        letterSleutel = sleutelZin[i]
-        # Transform eerste letter naar cijfer
-        Si = string.ascii_uppercase.index(letterSleutel)
-
-        Vi = Oi + Si % 26
-        print(string.ascii_uppercase[Vi])
-        # tekstVersleuteld.append(string.ascii_uppercase[Vi])
+        if letterTekst.isalpha():
+            # Transform letter naar cijfer
+            Oi = string.ascii_uppercase.index(letterTekst)
+            # Verkrijg letter van string
+            letterSleutel = sleutelZin[i]
+            # Transform letter naar cijfer
+            Si = string.ascii_uppercase.index(letterSleutel)
+            # Bereken ascii
+            Vi = (Oi + Si) % 26
+            tekstVersleuteld.append((string.ascii_uppercase[Vi]))
+        else:
+            tekstVersleuteld.append(letterTekst)
         i = i + 1
 
-    # print(tekstVersleuteld)
+    return tekstVersleuteld
 
-# def decodeer(tekst: str, sleutelwoord: str) -> None:
-#     print("decodeer")
+
+def getTekstUnlocked(tekst, sleutelZin):
+    tekstUnlocked = []
+    i = 0
+    while i < len(tekst):
+        # Verkrijg letter van string
+        letterTekst = tekst[i]
+
+        if letterTekst.isalpha():
+            # Transform letter naar cijfer
+            Vi = string.ascii_uppercase.index(letterTekst)
+            # Verkrijg letter van string
+            letterSleutel = sleutelZin[i]
+            # Transform letter naar cijfer
+            Si = string.ascii_uppercase.index(letterSleutel)
+            # Bereken ascii
+            Oi = (Vi - Si) % 26
+            tekstUnlocked.append((string.ascii_uppercase[Oi]))
+        else:
+            tekstUnlocked.append(letterTekst)
+        i = i + 1
+
+    return tekstUnlocked
 
 def getLengteTekst(tekst):
-    # Verwijder spaties van string
-    tekst = tekst.replace(" ", "")
-    # Verwijder uitroeptekens van string
-    tekst = tekst.replace("!", "")
     lengte = len(tekst)
-    return lengte, tekst
+    return lengte
 
 def getLengteSleutelwoord(sleutelwoord):
     lengte = len(sleutelwoord)
@@ -68,6 +90,18 @@ def getSleutelzin(sleutelwoord, aantalkeer_passend, overige_letters):
     zin = sleutelwoord * aantalkeer_passend + sleutelwoord[0:overige_letters]
     return zin
 
+def main():
+    # Codeer 1
+    codeer('NOBODY EXPECTS THE SPANISH INQUISITION!', 'CIRCUS')
+
+    # Decodeer 1
+    decodeer('PWSQXQ MORYUVA VBW AGCHAUP KHIWQJKNAQV!', 'CIRCUS')
+
+    # Codeer 2
+    codeer('OH SHUT UP! AND GO AND CHANGE YOUR ARMOUR!', 'ARTHUR')
+
+    # Decodeer 2
+    decodeer('OY ZBLT NW! AEW AF RGK THRGNY YFNY RRDHBL!', 'ARTHUR')
 
 if __name__ == "__main__":
     main()
