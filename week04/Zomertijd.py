1cd3423db0bc14a26c5f7eb41c4e7cc34c1e235a
Luquatic <jesseyfransen@live.nl>
2016-12-13 11:46:32 +0100

    added Zomertijd

--- week04/Zomertijd.py
+++ week04/Zomertijd.py
@@ -2,3 +2,25 @@
 Opdracht 20 - Zomertijd
 https://dodona.ugent.be/nl/exercises/116986379/
 """
+
+import calendar
+from datetime import date, timedelta
+
+def wintertijd():
+    print("wintertijd")
+
+def zomertijd(input: int):
+    cal = calendar.Calendar(0)
+    month = cal.monthdatescalendar(input, 3)
+    lastweek = month[-1]
+    sunday = lastweek[6]
+    print(sunday)
+
+def main():
+    vandaag = date.today()
+    morgen = vandaag + timedelta(1)
+
+    zomertijd(2013)
+
+if __name__ == "__main__":
+    main()
