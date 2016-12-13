f9905115f9e1422b7826e1e306743fa479b33b68
Luquatic <jesseyfransen@live.nl>
2016-12-13 11:45:55 +0100

    added Zoemzinnen

--- week04/Zoemzinnen.py
+++ week04/Zoemzinnen.py
@@ -5,21 +5,15 @@ https://dodona.ugent.be/nl/exercises/1620493816/
 
 import random
 
-def zoemzin1(a, b, c):
-    print(random.choice(a), random.choice(b), random.choice(c))
+def zoemzin1(shakespeare: list):
+    zin = []
+    for lijst in shakespeare:
+        zin.append(random.choice(lijst))
 
-def zoemzin2(a, b, c, *d):
-    if d == True:
-        random1 = random.choice(a)
-        random2 = random.choice(b)
-        random3 = random.choice(c)
-        random4 = random.choice(d)
-        print(random1 +" " +random2 +" "+random3 +" " +random4)
-    else:
-        random1 = random.choice(a)
-        random2 = random.choice(b)
-        random3 = random.choice(c)
-        print(random1 + " " + random2 + " " + random3)
+    print(' '.join(zin))
+
+def zoemzin2(*args):
+    zoemzin1(args)
 
 def main():
     buzz1 = ('integrated', 'total', 'systematized', 'parallel', 'functional', 'responsive', 'optional', 'synchronized', 'compatible', 'balanced')
@@ -31,7 +25,8 @@ def main():
     shakespeare3 = ['base-court', 'bat-fowling', 'beef-witted', 'beetle-headed', 'boil-brained', 'clapper-clawed', 'clay-brained', 'common-kissing', 'crook-pated', 'dismal-dreaming', 'dizzy-eyed', 'doghearted', 'dread-bolted', 'earth-vexing', 'elf-skinned', 'fat-kidneyed', 'fen-sucked', 'flap-mouthed', 'fly-bitten', 'folly-fallen', 'fool-born', 'full-gorged', 'guts-griping', 'half-faced', 'hasty-witted', 'hedge-born', 'hell-hated', 'idle-headed', 'ill-breeding', 'ill-nurtured', 'knotty-pated', 'milk-livered', 'motley-minded', 'onion-eyed', 'plume-plucked', 'pottle-deep', 'pox-marked', 'reeling-ripe', 'rough-hewn', 'rude-growing', 'rump-fed', 'shard-borne', 'sheep-biting', 'spur-galled', 'swag-bellied', 'tardy-gaited', 'tickle-brained', 'toad-spotted', 'urchin-snouted', 'weather-bitten']
     shakespeare4 = ['apple-john', 'baggage', 'barnacle', 'bladder', 'boar-pig', 'bugbear', 'bum-bailey', 'canker-blossom', 'clack-dish', 'clotpole', 'coxcomb', 'codpiece', 'death-token', 'dewberry', 'flap-dragon', 'flax-wench', 'flirt-gill', 'foot-licker', 'fustilarian', 'giglet', 'gudgeon', 'haggard', 'harpy', 'hedge-pig', 'horn-beast', 'hugger-mugger', 'jolthead', 'lewdster', 'lout', 'maggot-pie', 'malt-worm', 'mammet', 'measle', 'minnow', 'miscreant', 'moldwarp', 'mumble-news', 'nut-hook', 'pigeon-egg', 'pignut', 'puttock', 'pumpion', 'ratsbane', 'scut', 'skainsmate', 'strumpet', 'varlet', 'vassal', 'whey-face', 'wagtail']
 
-    zoemzin1(buzz1, buzz2, buzz3)
+    zoemzin1([shakespeare1, shakespeare2, shakespeare3, shakespeare4])
     zoemzin2(buzz1, buzz2, buzz3)
+
 if __name__ == "__main__":
     main()
