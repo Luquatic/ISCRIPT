54c72418c140e19be177bf79c0a78f10fef007b9
Luquatic <jesseyfransen@live.nl>
2016-12-14 21:50:08 +0100

    small changes

--- week04/Experimentele verjaardagsparadox.py
+++ week04/Experimentele verjaardagsparadox.py
@@ -2,3 +2,15 @@
 Opdracht 18 - Experimentele verjaardagsparadox
 https://dodona.ugent.be/nl/exercises/1257408557/
 """
+
+def happen_together(m: int, n: int):
+    print("happen")
+
+def estimate_chance(m: int, n: int, n3: int):
+    print("estimate")
+
+def main():
+    happen_together(6, 3)
+
+if __name__ == '__main__':
+    main()
