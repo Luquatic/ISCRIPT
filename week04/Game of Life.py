f74d23bebde2f288072c3454415f04b8bedf7af4
Luquatic <jesseyfransen@live.nl>
2016-12-14 17:02:07 +0100

    small changes

--- week04/Game of Life.py
+++ week04/Game of Life.py
@@ -2,3 +2,19 @@
 Opdracht 16 - Game of Life
 https://dodona.ugent.be/nl/exercises/511272034/
 """
+
+def showGeneration(generation):
+    print("showGeneration")
+
+def numberOfNeighbours(generation, n1: int, n2: int):
+    print("numberOfNeighbours")
+    nextGeneration()
+
+def nextGeneration():
+    print("nextGeneration")
+
+def main():
+    print("main")
+
+if __name__ == '__main__':
+    main()
