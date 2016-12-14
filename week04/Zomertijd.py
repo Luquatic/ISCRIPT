bb0887badf8f8589b60eeaf12bc3da1fe09c1985
Luquatic <jesseyfransen@live.nl>
2016-12-14 15:29:11 +0100

    added Zomertijd

--- week04/Zomertijd.py
+++ week04/Zomertijd.py
@@ -3,24 +3,43 @@ Opdracht 20 - Zomertijd
 https://dodona.ugent.be/nl/exercises/116986379/
 """
 
-import calendar
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
-def wintertijd():
-    print("wintertijd")
+def klok(input: str):
+    datum = datetime.strptime(input, '%d/%m/%Y')
+    datum = datetime.date(datum)
+
+    zomer = zomertijd(datum.year)
+    winter = wintertijd(datum.year)
+
+    if winter < datum and datum > zomer:
+        print("wintertijd")
+
+    elif winter > datum and datum > zomer:
+        print("zomertijd")
+
+    elif datum == wintertijd(datum.year):
+        print("omschakeling van zomertijd naar wintertijd.")
+
+    elif datum == zomertijd(datum.year):
+        print("omschakeling van wintertijd naar zomertijd.")
+
+def wintertijd(input: int):
+    jaar = input
+    dag = date(jaar, 11, 1) - timedelta(1)
+    while dag.weekday() is not 6:
+        dag = dag - timedelta(1)
+    return dag
 
 def zomertijd(input: int):
-    cal = calendar.Calendar(0)
-    month = cal.monthdatescalendar(input, 3)
-    lastweek = month[-1]
-    sunday = lastweek[6]
-    print(sunday)
+    jaar = input
+    dag = date(jaar, 4, 1) - timedelta(1)
+    while dag.weekday() is not 6:
+        dag = dag - timedelta(1)
+    return dag
 
 def main():
-    vandaag = date.today()
-    morgen = vandaag + timedelta(1)
-
-    zomertijd(2013)
+    klok('27/06/2013')
 
 if __name__ == "__main__":
     main()
