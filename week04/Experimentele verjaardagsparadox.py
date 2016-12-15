0a493968dc54497a5fc14f9a819e1f96ee034a31
Luquatic <jesseyfransen@live.nl>
2016-12-15 16:25:55 +0100

    small edits

--- week04/Experimentele verjaardagsparadox.py
+++ week04/Experimentele verjaardagsparadox.py
@@ -3,10 +3,16 @@ Opdracht 18 - Experimentele verjaardagsparadox
 https://dodona.ugent.be/nl/exercises/1257408557/
 """
 
-def happen_together(m: int, n: int):
-    print("happen")
+import random
 
-def estimate_chance(m: int, n: int, n3: int):
+def happen_together(m: int, n: int):
+    duplicates = []
+
+    for i in range(0, n):
+        duplicates.append(random.randint(1, m))
+
+
+def estimate_chance(m: int, n: int, tests: int):
     print("estimate")
 
 def main():
