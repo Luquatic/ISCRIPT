29b6b8b886d9ff6ecb3d0715d385392072fa1f38
Luquatic <jesseyfransen@live.nl>
2016-12-16 14:05:21 +0100

    added Game of Life

--- week04/Game of Life.py
+++ week04/Game of Life.py
@@ -3,19 +3,66 @@ Opdracht 16 - Game of Life
 https://dodona.ugent.be/nl/exercises/511272034/
 """
 
-def showGeneration(generation):
-    print("showGeneration")
+def showGeneration(tabel):
+    for row in tabel:
+        line = ""
+        for cel in row:
+            if cel:
+                line += "X"
+            else:
+                line += "O"
+            line += " "
 
-def numberOfNeighbours(generation, n1: int, n2: int):
-    print("numberOfNeighbours")
-    nextGeneration()
+        print(line)
 
-def nextGeneration():
-    print("nextGeneration")
+def numberOfNeighbours(tabel, x, y):
+    neighbours = 0
+    for xOff in range(-1, 2):
+        for yOff in range(-1, 2):
+            xNeighbour = x + xOff
+            yNeighbour = y + yOff
+            if (not ((xOff == 0) & (yOff == 0))) & (xNeighbour >= 0) & (yNeighbour >= 0) & (xNeighbour < len(tabel)) & \
+                    (yNeighbour < len(tabel[0])):
+                if tabel[xNeighbour][yNeighbour]:
+                    neighbours += 1
+    return neighbours
+
+def nextGeneration(tabel):
+    ret = [[False] * len(tabel[0]) for _ in range(len(tabel))]
+    for rowIndex in range(len(tabel)):
+        row = tabel[rowIndex]
+        for celIndex in range(len(row)):
+            cel = row[celIndex]
+            neighbours = numberOfNeighbours(tabel, rowIndex, celIndex)
+            if neighbours == 2:
+                ret[rowIndex][celIndex] = cel
+            elif neighbours == 3:
+                ret[rowIndex][celIndex] = True
+            else:
+                ret[rowIndex][celIndex] = False
+    return ret
+
 
 def main():
-    print("main")
+    generation = [[True] + [False] * 7 for _ in range(6)]
+    showGeneration(generation)
+
+    print(numberOfNeighbours(generation, 0, 0))
+    print(numberOfNeighbours(generation, 1, 1))
+    print(numberOfNeighbours(generation, 2, 2))
+    print()
+
+    next = nextGeneration(generation)
+    showGeneration(next)
+    print()
+    next = nextGeneration(next)
+    showGeneration(next)
+    print()
+    next = nextGeneration(next)
+    showGeneration(next)
+    print()
+    next = nextGeneration(next)
+    showGeneration(next)
 
 if __name__ == '__main__':
     main()
-
