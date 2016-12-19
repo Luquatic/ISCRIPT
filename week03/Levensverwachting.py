dcd0f9c9a0cf35ba2160e0a019397f8cf9cce501
Luquatic <jesseyfransen@live.nl>
2016-12-19 14:46:04 +0100

    improved Levensverwachting

--- week03/Levensverwachting.py
+++ week03/Levensverwachting.py
@@ -3,86 +3,62 @@ Opdracht 14 - Levensverwachting
 https://dodona.ugent.be/nl/exercises/849566952/
 """
 
-global levensverwachting
+def berekenGeslacht(leeftijd, geslacht: str):
+    if geslacht == 'man':
+        return leeftijd
+    else:
+        leeftijd = leeftijd + 4
+        return leeftijd
+
+def berekenRoker(leeftijd, roker: bool):
+    if roker == True:
+        leeftijd = leeftijd - 5
+        return leeftijd
+    else:
+        leeftijd = leeftijd + 5
+        return leeftijd
+
+def berekenSport(leeftijd, sport: int):
+    if sport == 0:
+        leeftijd = leeftijd - 3
+        return leeftijd
+    elif sport > 0:
+        leeftijd = leeftijd + sport
+        return leeftijd
+
+def berekenAlcohol(leeftijd, alcohol: int):
+    if alcohol == 0:
+        leeftijd = leeftijd + 2
+        return leeftijd
+    elif alcohol <= 7:
+        return leeftijd
+    else:
+        newAlcohol = (alcohol - 7) * 0.5
+        leeftijd = leeftijd - newAlcohol
+        return leeftijd
+
+def berekenFastfood(leeftijd, fastfood: bool):
+    if fastfood == True:
+        return leeftijd
+    else:
+        leeftijd = leeftijd + 3
+        return leeftijd
+
+def levensverwachting(geslacht: str, roker: bool, sport: int, alcohol: int, fastfood: bool):
+    leeftijd = 70
+    leeftijd = berekenGeslacht(leeftijd, geslacht)
+    leeftijd = berekenRoker(leeftijd, roker)
+    leeftijd = berekenSport(leeftijd, sport)
+    leeftijd = berekenAlcohol(leeftijd, alcohol)
+    leeftijd = berekenFastfood(leeftijd, fastfood)
+    print('%.1f' % leeftijd)
 
 def main():
-    levensverwachting = 70
-    levensverwachting = getGeslacht(levensverwachting)
-    levensverwachting = getRoker(levensverwachting)
-    levensverwachting = getSport(levensverwachting)
-    levensverwachting = getAlcohol(levensverwachting)
-    levensverwachting = getFastfood(levensverwachting)
-    getLevensverwachting(levensverwachting)
+    levensverwachting(geslacht='man', roker=True, sport=2, alcohol=10, fastfood=True)
+    levensverwachting(geslacht='man', roker=True, sport=5, alcohol=5, fastfood=True)
+    levensverwachting(geslacht='vrouw', roker=False, sport=5, alcohol=0, fastfood=False)
+    levensverwachting(geslacht='vrouw', roker=False, sport=3, alcohol=14, fastfood=True)
+    levensverwachting(geslacht='man', roker=False, sport=4, alcohol=4, fastfood=False)
 
-def getGeslacht(levensverwachting):
-    geslacht = input("Man of vrouw? ")
-    if geslacht == "man":
-        return levensverwachting
-    elif geslacht == "vrouw":
-        levensverwachting = levensverwachting + 4
-        return levensverwachting
-    else:
-        print("error")
-        quit()
-
-def getRoker(levensverwachting):
-    roker = input("Rookt u? ")
-    if roker == "ja":
-        roker = True
-        levensverwachting = levensverwachting - 5
-        return levensverwachting
-    elif roker == "nee":
-        roker = False
-        levensverwachting = levensverwachting + 5
-        return levensverwachting
-    else:
-        print("error")
-        quit()
-
-def getSport(levensverwachting):
-    sport = int(input("Hoeveel uur in de week sport u? "))
-    if sport == 0:
-        levensverwachting = levensverwachting - 3
-        return levensverwachting
-    elif sport > 0:
-        levensverwachting = levensverwachting + sport
-        return levensverwachting
-    else:
-        print("error")
-        quit()
-
-def getAlcohol(levensverwachting):
-    alcohol = int(input("Hoeveel glazen alcohol in de week drinkt u? "))
-    if alcohol == 0:
-        levensverwachting = levensverwachting + 2
-        return levensverwachting
-    elif alcohol <= 7:
-        return levensverwachting
-    elif alcohol > 7:
-        newAlcohol = (alcohol - 7) * 0.5
-        levensverwachting = levensverwachting - newAlcohol
-        return levensverwachting
-    else:
-        print("error")
-        quit()
-
-def getFastfood(levensverwachting):
-    fastfood = input("Eet u vaak fastfood? ")
-    if fastfood == "ja":
-        fastfood = True
-        return levensverwachting
-    elif fastfood == "nee":
-        fastfood = False
-        levensverwachting = levensverwachting + 3
-        return levensverwachting
-    else:
-        print("error")
-        quit()
-
-def getLevensverwachting(levensverwachting):
-    print(float(levensverwachting))
-
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
