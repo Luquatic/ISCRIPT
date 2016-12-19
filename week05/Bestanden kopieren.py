cc1f3c335aba49eb6019d4d4dc2b76f72681ca2f
Luquatic <jesseyfransen@live.nl>
2016-12-19 23:07:19 +0100

    added Bestanden kopieren

--- week05/Bestanden kopieren.py
+++ week05/Bestanden kopieren.py
@@ -3,10 +3,13 @@ Opdracht 24
 https://dodona.ugent.be/nl/exercises/1985652400/
 """
 
+import shutil
+
+def copy(source, destination):
+    shutil.copyfile(source, destination)
 
 def main():
-    print("main")
-
+    copy("original.txt", "copy.txt")
 
 if __name__ == '__main__':
     main()
