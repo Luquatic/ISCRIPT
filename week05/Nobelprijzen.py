0a4fe6bce5052f597ac88a2220525b19d5bfec62
Luquatic <jesseyfransen@live.nl>
2016-12-20 12:01:20 +0100

    small changes

--- week05/Nobelprijzen.py
+++ week05/Nobelprijzen.py
@@ -5,17 +5,40 @@ https://dodona.ugent.be/nl/exercises/1392321201/
 
 import csv
 
-def prijzen(file: str, prijs="", discipline="", jaar=0, nationaliteit="", laureaten=0, motivering=""):
+def eerste(file:str, prijs=None, jaar=None):
     with open(file) as file:
         fileRead = csv.reader(file, delimiter=";")
         for row in fileRead:
             if prijs in row:
                 if str(jaar) in row:
-                        print(row[0], "voor de", row[1], "(" +row[2] +")", row[3])
+                    print(row[0], "voor de", row[1], "(" +row[2] +")", row[3])
+
+def tweede(file:str, nationaliteit=None):
+    with open(file) as file:
+        fileRead = csv.reader(file, delimiter=";")
+        for row in fileRead:
+            if nationaliteit is not None:
+                if any(nationaliteit in s for s in row):
+                    print(row[0], "voor de", row[1], "(" + row[2] + ")", row[3])
+
+def derde(file:str, discipline=None, laureaten=None):
+    with open(file) as file:
+        fileRead = csv.reader(file, delimiter=";")
+        for row in fileRead:
+            if discipline in row:
+
+                    print(row[0], "voor de", row[1], "(" + row[2] + ")", row[3])
+
+def prijzen(file: str, prijs=None, discipline=None, jaar=None, nationaliteit=None, laureaten=None, motivering=None):
+    eerste(file, prijs, jaar)
+    tweede(file, nationaliteit)
 
 
 def main():
     prijzen('prijzen.csv', prijs='Nobelprijs', jaar=1994)
+    prijzen('prijzen.csv', prijs='Nobelprijs', discipline='wiskunde')
+    prijzen('prijzen.csv', nationaliteit='Bel')
+    prijzen('prijzen.csv', discipline='scheikunde', laureaten=3)
 
 if __name__ == '__main__':
     main()
